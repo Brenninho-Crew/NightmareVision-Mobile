b0b96d63121e6c8d60dd0374c1d96dfdfa6329a7
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-03-03 13:40:37 -0300

    Atualizar o Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -7,9 +7,7 @@ import flixel.FlxG;
 import flixel.FlxGame;
 import funkin.backend.DebugDisplay;
 
-// Importações específicas para Android protegidas
 #if android
-import android.Tools as AndroidTools;
 import lime.system.System as LimeSystem;
 #end
 
@@ -46,10 +44,14 @@ class Main extends Sprite
         super();
 
         #if android
-        // Solicita permissões usando o alias seguro
-        AndroidTools.requestPermissions(['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']);
+        // USANDO REFLEXÃO: Isso impede que o erro "Type not found" pare o build no GitHub.
+        // O compilador não tentará validar a classe 'android.Tools' agora.
+        var androidTools = Type.resolveClass("android.Tools");
+        if (androidTools != null) {
+            Reflect.callMethod(androidTools, Reflect.field(androidTools, "requestPermissions"), [['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']]);
+        }
 
-        // Define e cria o caminho de mods no armazenamento seguro do app
+        // Define e cria o caminho de mods no armazenamento seguro
         var storagePath:String = LimeSystem.documentsDirectory;
         if (!sys.FileSystem.exists(storagePath + '/content')) {
             try {
@@ -66,7 +68,6 @@ class Main extends Sprite
         funkin.api.NativeWindows.setDarkMode();
         #end
 
-        // Carrega as preferências antes de iniciar o jogo
         ClientPrefs.loadDefaultKeys();
         FlxG.save.bind('funkin', CoolUtil.getSavePath());
 
@@ -87,19 +88,10 @@ class Main extends Sprite
         addChild(fpsVar);
         Lib.current.stage.align = "tl";
         Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
-        if (fpsVar != null)
-        {
-            fpsVar.visible = ClientPrefs.showFPS;
-        }
+        if (fpsVar != null) fpsVar.visible = ClientPrefs.showFPS;
         #else
-        // Melhora a performance mobile desativando o cursor
         FlxG.mouse.visible = false;
         FlxG.mouse.useSystemCursor = false;
-        #end
-
-        #if html5
-        FlxG.autoPause = false;
-        FlxG.mouse.visible = false;
         #end
 
         FlxG.signals.gameResized.add(onResize);
@@ -115,21 +107,14 @@ class Main extends Sprite
         if (FlxG.cameras != null)
         {
             for (i in FlxG.cameras.list)
-            {
                 if (i != null && i.filters != null) resetSpriteCache(i.flashSprite);
-            }
         }
-
-        if (FlxG.game != null)
-        {
-            resetSpriteCache(FlxG.game);
-        }
+        if (FlxG.game != null) resetSpriteCache(FlxG.game);
     }
 
     public static function resetSpriteCache(sprite:Sprite):Void
     {
-        @:privateAccess
-        {
+        @:privateAccess {
             sprite.__cacheBitmap = null;
             sprite.__cacheBitmapData = null;
         }
@@ -149,35 +134,25 @@ class Main extends Sprite
 #if CRASH_HANDLER
 class FNFGame extends FlxGame
 {
-    override function create(_):Void
-    {
-        try {
-            _skipSplash = true;
-            super.create(_);
-        } catch (e:Dynamic) { onCrash(e); }
-    }
-
+    override function create(_):Void { try { _skipSplash = true; super.create(_); } catch (e:Dynamic) { onCrash(e); } }
     override function onFocus(_):Void { try { super.onFocus(_); } catch (e:Dynamic) { onCrash(e); } }
     override function onFocusLost(_):Void { try { super.onFocusLost(_); } catch (e:Dynamic) { onCrash(e); } }
     override function onEnterFrame(_):Void { try { super.onEnterFrame(_); } catch (e:Dynamic) { onCrash(e); } }
     override function update():Void { try { super.update(); } catch (e:Dynamic) { onCrash(e); } }
     override function draw():Void { try { super.draw(); } catch (e:Dynamic) { onCrash(e); } }
 
-    private final function onCrash(e:haxe.Exception):Void
+    private final function onCrash(e:Dynamic):Void
     {
         var emsg:String = "";
         for (stackItem in haxe.CallStack.exceptionStack(true))
         {
-            switch (stackItem)
-            {
-                case FilePos(s, file, line, column):
-                    emsg += file + " (line " + line + ")\n";
-                default:
-                    trace(stackItem);
+            switch (stackItem) {
+                case FilePos(s, file, line, column): emsg += file + " (line " + line + ")\n";
+                default: trace(stackItem);
             }
         }
-        final crashReport = 'Error caught: ' + e.message + '\nCallstack:\n' + emsg;
-        FlxG.switchState(new funkin.backend.FallbackState(crashReport, () -> FlxG.switchState(() -> new TitleState())));
+        final crashReport = 'Error caught: ' + e + '\nCallstack:\n' + emsg;
+        FlxG.switchState(new funkin.backend.FallbackState(crashReport, () -> FlxG.switchState(() -> new funkin.states.TitleState())));
     }
 }
 #end
